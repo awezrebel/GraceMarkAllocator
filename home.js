5d588d64c9bbccdccedcc3edc46f5e513822da99
kruthikpavan <kruthikpavan24@gmail.com>
2021-08-21 16:26:42 +0530

    adding quotes api

--- home.js
+++ home.js
@@ -1,3 +1,7 @@
+const quotesy= document.querySelector(".quote")
+const author= document.querySelector(".author")
+const button=document.querySelector(".button");
+let num=Math.floor(Math.random() * 100);
 const contactUs= document.querySelector(".contact")
 const heady= document.querySelector(".head")
 
@@ -37,10 +41,23 @@ else{
             },500)
 
       
-        
-
- 
-    
+  
 }
 })
+
+
+async function jokes(){
+    const jokes = await fetch('https://type.fit/api/quotes');
+    const final= await jokes.json()
+    
+    quotesy.innerText= final[num].text;
+    author.innerText=`~${final[num].author}`;
+    num+=1
+
+    
+    
+
+}
+
+jokes();
 
